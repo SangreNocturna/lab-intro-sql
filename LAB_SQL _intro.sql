d101e3e894b69883240a3c1d4576e39af0ea47e9
SangreNocturna <jtafernandez@gmail.com>
2022-09-14 11:54:47 +0200

    LAB_SQL _intro.sql

---
+++ LAB_SQL _intro.sql
@@ -0,0 +1,16 @@
+USE sakila;
+
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+
+SELECT title FROM film;
+SELECT * FROM language;
+
+select * from store;
+select * from staff;
+select first_name from staff;
+
+-- SELECT * FROM sakila.actor 
+-- ORDER BY account_id ASC
+-- LIMIT 10;
